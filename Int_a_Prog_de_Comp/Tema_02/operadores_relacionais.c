8cc0a2c60bcd093dc292e48838ee30b48f0dcb49
Cleylton Alcântara <59146828+CleyltonAlcantara@users.noreply.github.com>
2025-06-06 14:08:12 -0300

    Estudo operadores relacionais

---
+++ Int_a_Prog_de_Comp/Tema_02/operadores_relacionais.c
@@ -0,0 +1,13 @@
+include <stdio.h>
+
+    int main (){
+        int maca = 76.0;
+        int laranja = 75.0;
+    
+        if (maca >= laranja);
+            printf ("Tem mais maca do que laranja");
+            else
+            printf ("Tem mais laranja do que maca");
+
+return=0;
+}
