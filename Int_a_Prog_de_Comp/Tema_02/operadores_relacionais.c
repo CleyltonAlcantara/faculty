1f95426d3f20777273e0bb9197d32335ed05e606
Cleylton Alcântara <59146828+CleyltonAlcantara@users.noreply.github.com>
2025-06-09 10:10:25 -0300

    Update operadores_relacionais.c

--- Int_a_Prog_de_Comp/Tema_02/operadores_relacionais.c
+++ Int_a_Prog_de_Comp/Tema_02/operadores_relacionais.c
@@ -1,13 +1,16 @@
 include <stdio.h>
 
     int main (){
-        int maca = 76.0;
-        int laranja = 75.0;
+       int numero1, numero2;
+
+    scanf ("Digite o numero 1\n");
+    scanf ("Digite o numero 2\n");
     
-        if (maca >= laranja);
-            printf ("Tem mais maca do que laranja");
+        if (numero1 > numero2){
+            printf ("O numero 1 é maior que o número 2\n");
             else
-            printf ("Tem mais laranja do que maca");
+            printf ("O número 1 é menor que o número 2\n");
+        }
 
 return=0;
 }
