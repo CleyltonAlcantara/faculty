d34c0d8e38d51b0bcf5134a95327b3d8ceed91af
CleyltonAlcantara <cleylton@gmail.com>
2025-10-08 17:26:25 -0300

    Refactor code structure for improved readability and maintainability

---
+++ Int_a_Prog_de_Comp/Tema_02/operadores_relacionais.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+    int main (){
+       int maça, laranja;
+
+    printf ("Digite a quantidade de maçã:");
+    scanf ("%d", &maça);
+    printf ("Digite a quantidade de laranja:");
+    scanf ("%d", &laranja);
+
+        if (maça >= laranja)
+            printf ("Tem mais maçã do que laranja!\n");
+            else
+            printf ("Tem mais laranja do que maçã!\n");
+
+return 0;
+}
